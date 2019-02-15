c2cd3475d2ec138fbe910c9f896888040dd7aa17
Diana Pop <32615142+PopDiana@users.noreply.github.com>
2019-02-15 19:46:56 +0200

    Add files via upload

---
+++ SleepingElves/OwnBarrier/Planner.java
@@ -0,0 +1,14 @@
+
+public class Planner {
+
+	public static void main(String[] args) {
+		
+		GiftTransfer giftQueue = new GiftTransfer();
+		SantaClaus Santa = new SantaClaus(giftQueue);
+		Workshop workshop =  new Workshop(giftQueue);
+		workshop.createFactories();
+		Santa.start();
+
+	}
+
+}
